77a5968927eb2fbfb8dfe27fd640a6329d2b5795
menna ramadan <mennaramadanabdallah@gmail.com>
2026-07-16 21:07:27 +0300

    maintain

---
+++ Migrations/20260716155722_ocd.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HubClub.Migrations
+{
+    /// <inheritdoc />
+    public partial class ocd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
